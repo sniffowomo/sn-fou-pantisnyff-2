fbb39fdad335b229dd0779aacf52c6f4ab5f953f
sniffowomo <123172291+sniffowomo@users.noreply.github.com>
2023-02-17 23:52:48 +0000

    💅🏻::SFP2

--- s/fo.sh
+++ s/fo.sh
@@ -2,3 +2,8 @@
 curl -L https://foundry.paradigm.xyz | bash
 source ~/.bashrc  
 foundryup 
+mkdir -p $HOME/.config/fish/completions
+forge completions fish > $HOME/.config/fish/completions/forge.fish
+cast completions fish > $HOME/.config/fish/completions/cast.fish
+anvil completions fish > $HOME/.config/fish/completions/anvil.fish
+source $HOME/.config/fish/config.fish
